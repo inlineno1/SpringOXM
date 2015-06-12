7966a536f139e427e75323103a75ab3e881f0ef8
inlineno1 <inlineno1@gmail.com>
2015-06-12 13:23:23 +0900

    modify

--- src/main/java/oxm/example/Application.java
+++ src/main/java/oxm/example/Application.java
@@ -9,6 +9,7 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        
         System.out.println("Spring Boot Started.....");
     }
 
